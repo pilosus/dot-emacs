add768e8aa45697545d160a022a9253b86c5d62f
Vitaly Samigullin <vrs@pilosus.org>
2023-12-26 14:40:46 +0100

    Don't ask for creds when using Tramp

--- init.el
+++ init.el
@@ -156,6 +156,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(auth-source-save-behavior nil) ;; don't ask for creds when using Tramp
  '(coffee-tab-width 2)
  '(package-selected-packages
    '(terraform-mode pyenv-mode company lsp-mode go-mode restclient elixir-mode dockerfile-mode slime clj-refactor markdown-mode yaml-mode which-key magit tagedit rainbow-delimiters projectile smex ido-completing-read+ cider clojure-mode-extra-font-locking clojure-mode paredit exec-path-from-shell)))
