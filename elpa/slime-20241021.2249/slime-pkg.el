81f330ca32ed93042ae8c38969bdfde372c4e35e
Vitaly Samigullin <vrs@pilosus.org>
2025-03-25 19:49:58 +0100

    M-x package-upgrade-all 25.03.2025

--- elpa/slime-20241021.2249/slime-pkg.el
+++
@@ -1,9 +0,0 @@
-;; -*- no-byte-compile: t; lexical-binding: nil -*-
-(define-package "slime" "20241021.2249"
-  "Superior Lisp Interaction Mode for Emacs."
-  '((emacs     "24.3")
-    (macrostep "0.9"))
-  :url "https://github.com/slime/slime"
-  :commit "58f71f42668ba52b46d140342f3c4b3820d6b309"
-  :revdesc "58f71f42668b"
-  :keywords '("languages" "lisp" "slime"))
